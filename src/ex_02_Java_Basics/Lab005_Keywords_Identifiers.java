476d1cac46caee9c224a84cc92a81bc4ef451386
Shravan Kakade <shravankakade@outlook.com>
2025-12-18 08:48:34 +0530

    Keywords and Identifiers

---
+++ src/ex_02_Java_Basics/Lab005_Keywords_Identifiers.java
@@ -0,0 +1,29 @@
+package ex_02_Java_Basics;
+
+public class Lab005_Keywords_Identifiers {
+//    Covered Lab009 to Lab012 in this Class
+    static void main() {
+
+        System.out.println("Keywords in the program");
+/*
+>   package, public, class, static, void
+>   these 5 are the keywords present in simple program
+Q.  How many Keywords are present in Java 24?
+A.  Java JDK has 54 reserved Keywords
+    Also it has 14 Contextual Keywords (Introduced in Modern Java)
+*/
+
+        System.out.println("Identifiers");
+/*
+*   These are the names that will be used to identify the programming elements like classes, methods and variables.
+
+*   Rules to follow when defining identifier -
+        > Identifier can contain Alphabets, digits & two special characters $ and _
+        > First character of identifier must be Alphabet or special chars i,e. $ or _
+        > Keywords can not be used as identifiers
+*/
+
+        System.out.println("Naming convention of the class");
+//    Always use CamelCase while creating class i,e. first letter of each word should be capital
+    }
+}
---
+++ src/ex_02_Java_Basics/Lab006_Println_Vs_Print.java
@@ -0,0 +1,11 @@
+package ex_02_Java_Basics;
+
+public class Lab006_Println_Vs_Print {
+    static void main() {
+        System.out.print("If ln is not added in print command then new line will not be added in o/p  ");
+        System.out.println("If ln is added in print command then new line will be added in o/p");
+        System.out.println("In the next line");
+
+
+    }
+}
