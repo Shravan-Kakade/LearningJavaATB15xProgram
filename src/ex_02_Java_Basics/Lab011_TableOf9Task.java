9a0f9fb2b55b5c06d37532e197186b77b3c01ebb
Shravan Kakade <shravankakade@outlook.com>
2025-12-18 23:22:40 +0530

    Next line

--- src/ex_02_Java_Basics/Lab011_TableOf9Task.java
+++ src/ex_02_Java_Basics/Lab011_TableOf9Task.java
@@ -4,15 +4,15 @@ public class Lab011_TableOf9Task {
     static void main() {
         int Table = 9;
         System.out.println("Printing table of 9 in the console using Printf method");
-        System.out.printf("%d * 1 = %d ", Table, Table*1);
-        System.out.printf("%d * 2 = %d ", Table, Table*2);
-        System.out.printf("%d * 3 = %d ", Table, Table*3);
-        System.out.printf("%d * 4 = %d ", Table, Table*4);
-        System.out.printf("%d * 5 = %d ", Table, Table*5);
-        System.out.printf("%d * 6 = %d ", Table, Table*6);
-        System.out.printf("%d * 7 = %d ", Table, Table*7);
-        System.out.printf("%d * 8 = %d ", Table, Table*8);
-        System.out.printf("%d * 9 = %d ", Table, Table*9);
-        System.out.printf("%d * 10 = %d ", Table, Table*10);
+        System.out.printf("%d * 1 = %d \n", Table, Table*1);
+        System.out.printf("%d * 2 = %d \n", Table, Table*2);
+        System.out.printf("%d * 3 = %d \n", Table, Table*3);
+        System.out.printf("%d * 4 = %d \n", Table, Table*4);
+        System.out.printf("%d * 5 = %d \n", Table, Table*5);
+        System.out.printf("%d * 6 = %d \n", Table, Table*6);
+        System.out.printf("%d * 7 = %d \n", Table, Table*7);
+        System.out.printf("%d * 8 = %d \n", Table, Table*8);
+        System.out.printf("%d * 9 = %d \n", Table, Table*9);
+        System.out.printf("%d * 10 = %d \n", Table, Table*10);
     }
 }
