e7cc89cf050edcccb5818b2438020fbde27f8026
Shravan Kakade <shravankakade@outlook.com>
2025-12-18 07:50:13 +0530

    Comments and Main Method

---
+++ src/ex_02_Java_Basics/Lab003_No_Main_Method.java
@@ -0,0 +1,8 @@
+package ex_02_Java_Basics;
+
+public class Lab003_No_Main_Method {
+    // when no main method is added in program, it can not be executed
+    static void main() {
+        System.out.println("with the help of println command, output can be printed to the console");
+    }
+}
---
+++ src/ex_02_Java_Basics/Lab004_Comments.java
@@ -0,0 +1,24 @@
+package ex_02_Java_Basics;
+
+public class Lab004_Comments {
+    //  > this is single line comment
+    //  > commented code will not be executed while executing program
+
+    /*
+        *   To add multiline comment this syntax is used
+        *   Multiline comment is used for documentation about code
+        *   While programming combination of both single line and multiline comment can be used
+        *   This class covered lab exercises from 04 to 08
+    */
+
+    public static void main() {
+
+        System.out.println("Added statement before comment");
+        // After adding comment or before it, program can be written
+        System.out.println("Adding statement after comment");
+
+        System.out.println("Shortcuts for adding comments are as follows");
+        System.out.println("using ctrl + / to add single line comment");
+        System.out.println("using ctrl + shift + / to add multi line comment");
+    }
+}
