9b20c0069096fdfb913bc044cd3535842cb82674
Shravan Kakade <shravankakade@outlook.com>
2025-12-20 23:10:03 +0530

    Table of 2 using Printf Practise

---
+++ src/ex_02_Java_Basics/Lab011_TableOf2Practise.java
@@ -0,0 +1,17 @@
+package ex_02_Java_Basics;
+
+public class Lab011_TableOf2Practise {
+    static void main() {
+        int Num = 2;
+        System.out.printf("%d * 1 = %d\n", Num, Num * 1);
+        System.out.printf("%d * 2 = %d\n", Num, Num * 2);
+        System.out.printf("%d * 3 = %d\n", Num, Num * 3);
+        System.out.printf("%d * 4 = %d\n", Num, Num * 4);
+        System.out.printf("%d * 5 = %d\n", Num, Num * 5);
+        System.out.printf("%d * 6 = %d\n", Num, Num * 6);
+        System.out.printf("%d * 7 = %d\n", Num, Num * 7);
+        System.out.printf("%d * 8 = %d\n", Num, Num * 8);
+        System.out.printf("%d * 9 = %d\n", Num, Num * 9);
+        System.out.printf("%d * 10 = %d\n", Num, Num * 10);
+    }
+}
